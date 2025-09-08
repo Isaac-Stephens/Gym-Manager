f3cc6192c5429b90d6706fbfa7ed1b0c44ae64ba
Isaac Stephens <isaac.stephens@example.com>
2025-09-08 13:46:32 -0500

    Added ImGUI library

---
+++ build/CMakeFiles/gymman.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gymman.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Main application for the database management application
+#include <iostream>
+
+
+using namespace std;
+
+
+int main () {
+
+
+    return 0;
+}
