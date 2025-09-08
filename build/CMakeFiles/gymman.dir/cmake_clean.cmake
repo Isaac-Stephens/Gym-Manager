085c9f680b302c7f211a059cbf648d23dca61ed5
isaac <isaac@IsaacGram.localdomain>
2025-09-08 13:46:32 -0500

    Added ImGUI library

---
+++ build/CMakeFiles/gymman.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gymman.dir/main.cpp.o"
+  "CMakeFiles/gymman.dir/main.cpp.o.d"
+  "gymman"
+  "gymman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gymman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/isaac/school/fs25/Dadabases_CS2300/Assignments/Gym-Manager
+# Build directory: /home/isaac/school/fs25/Dadabases_CS2300/Assignments/Gym-Manager/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
