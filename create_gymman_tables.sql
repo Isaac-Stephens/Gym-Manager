092f2644e11721d5d5c05802dfa59baa31216ba3
Isaac Stephens <198599277+Isaac-Stephens@users.noreply.github.com>
2025-09-25 15:36:34 -0500

    started creating tables

---
+++ create_gymman_tables.sql
@@ -0,0 +1,131 @@
+-------------------------------- MEMBERS --------------------------------
+CREATE TABLE Members (
+    member_id int AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birth_date DATE,
+    membership_start_date DATE DEFAULT CURRENT_DATE,
+    email VARCHAR(100) UNIQUE,
+    sex VARCHAR(10) -- 'Male', 'Female'
+);
+
+CREATE TABLE PhoneNumbers (
+    phone_number_id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    phone_number_type VARCHAR(20), -- e.g., 'Home', 'Work', 'Mobile'
+    CONSTRAINT fk_phone_member FOREIGN KEY (member_id) REFERENCES Members(member_id) -- Only allow phone numbers if member ID already exists
+);
+
+
+-- MEMBER (1) [has] (N) EMERGENCY_CONTACT
+CREATE TABLE EmergencyContacts (
+    emergency_contact_id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    relationship VARCHAR(50),
+    phone_number VARCHAR(20) NOT NULL,
+    email VARCHAR(100),
+
+    CONSTRAINT fk_emergency_member FOREIGN KEY (member_id) REFERENCES Members(member_id)
+        ON DELETE CASCADE -- If member is deleted, their emeregency contacts do too.
+);
+
+-- MEMBER (0) [pays] (N) PAYMENT
+CREATE TABLE Payments (
+    payment_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    member_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date DATE DEFAULT CURRENT_DATE,
+
+    FOREIGN KEY (member_id) REFERENCES Members(member_id) 
+);
+
+--------------------------------  STAFF  --------------------------------
+CREATE TABLE Staff (
+    staff_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    ssn VARCHAR(50) UNIQUE, -- XXX-XX-XXXX
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    employment_date DATE DEFAULT CURRENT_DATE,
+    birth_date DATE,
+    staff_address VARCHAR(255)
+);
+
+-- Subtypes(Staff): Trainers
+CREATE TABLE Trainers (
+    staff_id INT PRIMARY KEY,
+    speciality VARCHAR(255),
+    
+    CONSTRAINT fk_trainer_staff FOREIGN KEY (staff_id) REFERENCES Staff(staff_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE TrainerCertifications (
+    trainer_cert_id INT AUTO_INCREMENT PRIMARY KEY,
+    staff_id INT NOT NULL,
+    certification_name VARCHAR(255) NOT NULL,
+
+    CONSTRAINT fk_cert_staff FOREIGN KEY (staff_id) REFERENCES Trainers(staff_id)
+        ON DELETE CASCADE
+);
+
+-- Trainer (1) [trains] (N) Members
+CREATE TABLE TrainerClients (
+    trainer_client_id INT AUTO_INCREMENT PRIMARY KEY,
+    trainer_id INT NOT NULL,
+    member_id INT NOT NULL,
+    client_start_date DATE DEFAULT CURRENT_DATE,
+    client_end_date DATE, -- NULL = onging contract
+    notes TEXT,
+
+    CONSTRAINT fk_trainer FOREIGN KEY (trainer_id) REFERENCES Trainers(staff_id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_client FOREIGN KEY (member_id) REFERENCES Members(member_id)
+        ON DELETE CASCADE,
+    CONSTRAINT uq_client_trainer UNIQUE (trainer_id, member_id)
+);
+
+-- Subtypes(Staff): Maintainence
+CREATE TABLE Maintainence (
+    staff_id INT PRIMARY KEY,
+-- TODO Maintains equipment relationship
+    CONSTRAINT fk_maintainence_staff FOREIGN KEY (staff_id) REFERENCES Staff(staff_id)
+        ON DELETE CASCADE
+);
+
+-- Subtypes(Staff): Managers
+CREATE TABLE Managers (
+    staff_id INT PRIMARY KEY,
+    shift_managed VARCHAR(20), -- Day, Mid, Night
+
+    CONSTRAINT fk_manager_staff FOREIGN KEY (staff_id) REFERENCES Staff(staff_id)
+        ON DELETE CASCADE
+);
+
+-- STAFF PAY TYPES
+CREATE TABLE Contractors (
+    staff_id INT PRIMARY KEY,
+    
+    CONSTRAINT fk_contractor_staff FOREIGN KEY (staff_id) REFERENCES Staff(staff_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Hourly_Employees (
+    staff_id INT PRIMARY KEY,
+    
+    CONSTRAINT fk_hourly_staff FOREIGN KEY (staff_id) REFERENCES Staff(staff_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Salary_Employees (
+    staff_id INT PRIMARY KEY,
+    
+    CONSTRAINT fk_salary_staff FOREIGN KEY (staff_id) REFERENCES Staff(staff_id)
+        ON DELETE CASCADE
+);
+
+-- TODO: Exercise Logs
+
+-- TODO: Machinery Tracking
